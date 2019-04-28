2007d4575585ce07ea9f205316ac0c893a84f672
gustavo c santos <gustavosantosixi@gmail.com>
2019-04-28 00:20:49 -0300

    Erro de Auxilio Criação Concertado, Metodo de Intercessao de Area Ruim

--- src/br/udesc/ceavi/pin/modulo1/model/Node.java
+++ src/br/udesc/ceavi/pin/modulo1/model/Node.java
@@ -1,22 +1,23 @@
 package br.udesc.ceavi.pin.modulo1.model;
 
+import br.udesc.ceavi.pin.modulo1.help.HelpLocator;
 import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
-
+    
     public static final int SIZE = 6;
-
+    
     private final String ID;
     private final float x;
     private final float y;
-
+    
     private static int idNaoUsuado = 1;
     private final Rectangle area;
-
+    
     private List<Egde> listDeEgdeQuePertenco = new ArrayList<>();
-
+    
     public Node(float x, float y) {
         this.ID = "" + idNaoUsuado;
         idNaoUsuado++;
@@ -24,7 +25,7 @@ public class Node {
         this.y = y;
         this.area = new Rectangle((int) this.x, (int) this.y, SIZE, SIZE);
     }
-
+    
     public Node(Node n) {
         this.ID = "" + idNaoUsuado;
         idNaoUsuado++;
@@ -32,7 +33,7 @@ public class Node {
         this.y = n.getY();
         this.area = new Rectangle((int) this.x, (int) this.y, SIZE, SIZE);
     }
-
+    
     public Node(float x, float y, int sizeNodo) {
         this.ID = "" + idNaoUsuado;
         idNaoUsuado++;
@@ -40,12 +41,12 @@ public class Node {
         this.y = y;
         this.area = new Rectangle((int) this.x, (int) this.y, sizeNodo, sizeNodo);
     }
-
+    
     @Override
     public String toString() {
         return x + " : " + y;
     }
-
+    
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -66,48 +67,41 @@ public class Node {
         }
         return this.ID == other.getID();
     }
-
+    
     public float getX() {
         return x;
     }
-
+    
     public float getY() {
         return y;
     }
-
+    
     public String getID() {
         return ID;
     }
-
+    
     public Rectangle getArea() {
         return new Rectangle(area);
     }
-
+    
     public boolean collideWithMyArea(float x2, float y2) {
-        x2 -= 3;
-        y2 -= 3;
-        for (int emX = 0; emX < 5; emX++) {
-            for (int emY = 0; emY < 5; emY++) {
-                if ((x2 + emX) == this.x && (y2 + emY) == this.y) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return new Rectangle((int) x2 - (SIZE * HelpLocator.getZOOM()) / 2,
+                (int) y2 - (SIZE * HelpLocator.getZOOM()) / 2,
+                SIZE * HelpLocator.getZOOM(), SIZE * HelpLocator.getZOOM()).intersects(this.area);
     }
-
+    
     public boolean IamInArea(int X, int Y, int W, int H) {
         return new Rectangle(X, Y, W, H).intersects(area);
     }
-
+    
     public void associarNodeEgde(Egde e) {
         this.listDeEgdeQuePertenco.add(e);
     }
-
+    
     public void quebrarAssociarNodeEgde(Egde e) {
         this.listDeEgdeQuePertenco.remove(e);
     }
-
+    
     public boolean haveLinkEgde() {
         return !listDeEgdeQuePertenco.isEmpty();
     }
